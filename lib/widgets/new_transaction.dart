38b3bd9310a002b1e7075a678bd2ae59a2d5e4ad
NguyenDucTuanUET <64391055+ductuanUET@users.noreply.github.com>
2022-10-03 23:09:30 +0700

    Update lanscape mode

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -16,7 +16,7 @@ class _NewTransactionState extends State<NewTransaction> {
   DateTime? _selectedDate;
 
   void _submitData() {
-    if(_amountController.text.isEmpty) {
+    if (_amountController.text.isEmpty) {
       return;
     }
     final enteredTitle = _titleController.text;
@@ -54,50 +54,56 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        elevation: 5,
-        child: Container(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              TextField(
-                decoration: InputDecoration(labelText: 'Title'),
-                controller: _titleController,
-              ),
-              TextField(
-                decoration: InputDecoration(labelText: 'Amount'),
-                controller: _amountController,
-                keyboardType: TextInputType.number,
-                onSubmitted: (_) => _submitData,
-              ),
-              Container(
-                height: 70,
-                child: Row(
-                  children: [
-                    Expanded(
-                        child: Text(_selectedDate == null
-                            ? 'No Date Chosen!'
-                            : 'Picked Date: ${DateFormat.yMd().format(_selectedDate!)}')),
-                    TextButton(
-                        onPressed: _presentDatePicker,
-                        child: Text('Choose Date'),
-                        style: TextButton.styleFrom(
-                          foregroundColor: Theme.of(context).primaryColor,
-                        ))
-                  ],
+    return SingleChildScrollView(
+      child: Card(
+          elevation: 5,
+          child: Container(
+            padding: EdgeInsets.only(
+                top: 10,
+                left: 10,
+                right: 10,
+                bottom: MediaQuery.of(context).viewInsets.bottom + 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                TextField(
+                  decoration: InputDecoration(labelText: 'Title'),
+                  controller: _titleController,
                 ),
-              ),
-              ElevatedButton(
-                child: Text(
-                  'Add Transaction',
-                  style: TextStyle(color: Colors.white),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Amount'),
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  onSubmitted: (_) => _submitData,
                 ),
-                style: TextButton.styleFrom(primary: Colors.purple),
-                onPressed: _submitData,
-              )
-            ],
-          ),
-        ));
+                Container(
+                  height: 70,
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: Text(_selectedDate == null
+                              ? 'No Date Chosen!'
+                              : 'Picked Date: ${DateFormat.yMd().format(_selectedDate!)}')),
+                      TextButton(
+                          onPressed: _presentDatePicker,
+                          child: Text('Choose Date'),
+                          style: TextButton.styleFrom(
+                            foregroundColor: Theme.of(context).primaryColor,
+                          ))
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  child: Text(
+                    'Add Transaction',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: TextButton.styleFrom(primary: Colors.purple),
+                  onPressed: _submitData,
+                )
+              ],
+            ),
+          )),
+    );
   }
 }
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -48,7 +48,15 @@ class TransactionList extends StatelessWidget {
                     subtitle: Text(
                       DateFormat.yMMMd().format(transactions[index].date!),
                     ),
-                    trailing: IconButton(
+                    trailing:  MediaQuery.of(context).size.width > 480 
+                    ? TextButton.icon(
+                      icon: Icon(Icons.delete), 
+                      label: Text('Delete'),
+                      style: TextButton.styleFrom(foregroundColor: Theme.of(context).errorColor),
+                      onPressed: () => deleteTx(transactions[index].id),
+                      ) 
+                    
+                    : IconButton(
                       icon: Icon(Icons.delete),
                       color: Theme.of(context).errorColor,
                       onPressed: () => deleteTx(transactions[index].id),
